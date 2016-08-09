4c06024144cfcf2d28c8c24b5250a0b4c7fee961
Zeyi Fan <fanzeyi1994@gmail.com>
2016-08-09 11:52:35 -0700

    web front now use API to show result.

--- web/js/app.js
+++ web/js/app.js
@@ -1,6 +1,13 @@
 {
-  let xhr;
-  let auto_complete = new autoComplete({
+  function escapeHTML(s) {
+    return s.replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  }
+
+  var xhr;
+  var auto_complete = new autoComplete({
     selector: '.search-input',
     minChars: 1,
     source: function(term, response) {
@@ -10,16 +17,43 @@
   });
 
 
-  let body = document.getElementsByTagName("body")[0];
+  var body = document.getElementsByTagName("body")[0];
 
-  let form = document.getElementById("search-form");
-  form.addEventListener("submit", (e) => {
+  var form = document.getElementById("search-form");
+  var query = $("#query-input");
+  var resultList = $("#search-result-list");
+  var completeList = $(".autocomplete-suggestions");
+
+  form.addEventListener("submit", function(e) {
     $(".search-box").animate({
       height: "60px"
     }, 250, function() {
-      body.classList.toggle("result");
-      body.classList.toggle("search");
+      body.classList.add("result");
+      body.classList.remove("search");
     });
+
+    completeList.css({
+      display: 'none'
+    });
+
+    $.getJSON("/api/query", {
+      query: query.val()
+    }, function(resp) {
+      resultList.empty();
+      for (item of resp) {
+        var entry = `<li class="search-result">
+          <h2>
+            <a href="${item.url}">${item.title}</a>
+          </h2>
+          <small class="meta">${escapeHTML(item.url)}</small>
+          <div class="summary">
+          </div>
+        </li>`
+
+        resultList.append(entry);
+      }
+    });
+
     e.preventDefault();
   });
 
